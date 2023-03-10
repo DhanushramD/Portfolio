eba9e8d370a97342f9a639967731881d0a4589b7
DhanushramD <ddhanushram2002@gmail.com>
2023-03-10 10:48:45 +0530

    Pushed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("mybtn");
-var span = document.getElementsByClassName("close")[0]; 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+const overlay = document.querySelector('#overlay');
+
+document.querySelector('#mybtn').addEventListener("click", () => {
+  overlay.style.display = "block";
+})
+
+document.querySelector('#close').addEventListener("click", () => {
+  overlay.style.display = "none";
+})
